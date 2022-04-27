fcfc13827dbd8f6fe5c38feece4b6d23312ee84e
Soso Ogan <soso737@hotmail.co.uk>
2022-04-27 13:43:37 +0100

    added input for user interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function App(): JSX.Element {
-  const [pastes, setPastes] = useState<any[]>([]);
+  interface entry {
+    "entry_id": number,
+    "title_text": string,
+    "summary_text": string,
+    "time": string
+  }
+  const [pastes, setPastes] = useState<entry[]>([]);
+  const [title, setTitle] = useState<string>('');
+  const [summary, setSummary] = useState<string>('');
 
   function handleSubmit() {
     console.log("would send text");
     //gather detail of new entry
     const newEntry = {
-      title: "How to be a jellyFish",
-      summary: "We have not figured out our play by play",
+      title: title,
+      summary: summary,
     };
     //send an HTTP post request to our API server with new entry
     const url = "http://localhost:4000/pastes";
@@ -39,11 +47,16 @@ export default function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Pastebin App</h1>
+      <input placeholder="new paste" value={title} onChange={(event) => setTitle(event.target.value)} />
+      <input placeholder="new summary" value={summary} onChange={(event) => setSummary(event.target.value)} />
+      <hr/>
+
+
       <button onClick={handleSubmit}>Submit text</button>
       <button onClick={handleAllSubmits}>View list of all submits</button>
 
       {pastes.map((item) => (
-        <li key={item.entry_id}>{item.title_text}</li>
+        <li key={item.entry_id}>{item.title_text}<br /> {item.summary_text}<hr/></li>
       ))}
     </div>
   );
